f4a59f21c5a34ea173c8f0424d8d95811906d328
Jacob Legorreta <jacobslegorreta@gmail.com>
2020-10-28 17:58:56 -0700

    initial

---
+++ build/precache-manifest.10509a2f042fd957720dbef5b0518634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95955b8f0f64336b44d8c0f1499a8f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79be6fc36425f3ab7f46",
+    "url": "/static/css/main.31f9b7b6.chunk.css"
+  },
+  {
+    "revision": "49749f6560d2846f2f49",
+    "url": "/static/js/2.b8ee5e2e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b8ee5e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79be6fc36425f3ab7f46",
+    "url": "/static/js/main.ab15e632.chunk.js"
+  },
+  {
+    "revision": "61dcb7c98868b935c879",
+    "url": "/static/js/runtime-main.c3c2bbdf.js"
+  }
+]);
